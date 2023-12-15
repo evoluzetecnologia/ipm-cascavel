a30fd5880f8dedeb56217a51aec8c6b272ebd57e
evoluze.tecnologia <121568795+evoluzetecnologia@users.noreply.github.com>
2023-12-15 16:15:29 -0300

    Atualizando Formulário

---
+++ Form/TruNaN/scripts.js
@@ -0,0 +1,18 @@
+// Sua função para carregar a animação
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('lottie-animation'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: false, // Alterado para não iniciar automaticamente
+  path: 'truNaN - Robo 2.json'
+});
+
+function showLoading() {
+  document.getElementById('overlay').style.display = 'flex';
+  animation.play(); // Inicia a animação
+}
+
+function hideLoading() {
+  document.getElementById('overlay').style.display = 'none';
+  animation.stop(); // Para a animação
+}
